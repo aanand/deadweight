e7568f85f29df41da4e6e13cb16ddb57281daa0a
Aanand Prasad <aanand.prasad@gmail.com>
2009-07-21 14:27:26 -0400

    ~here it is~

---
+++ lib/deadweight.rb
@@ -0,0 +1,110 @@
+require 'css_parser'
+require 'hpricot'
+require 'open-uri'
+require 'logger'
+
+class Deadweight
+  attr_accessor :root, :stylesheets, :pages, :ignore_selectors, :mechanize, :log_file
+
+  def initialize
+    @root = 'http://localhost:3000'
+    @stylesheets = []
+    @pages = []
+    @ignore_selectors = []
+    @mechanize = false
+    @log_file = STDERR
+  end
+
+  def run
+    css = CssParser::Parser.new
+
+    @stylesheets.each do |path|
+      css.add_block!(fetch(path))
+    end
+
+    unused_selectors = []
+    total_selectors = 0
+
+    css.each_selector do |selector, declarations, specificity|
+      unless unused_selectors.include?(selector)
+        total_selectors += 1
+        unused_selectors << selector unless selector =~ ignore_selectors
+      end
+    end
+
+    pages.each do |page|
+      case page
+      when String
+        html = fetch(page)
+      else
+        result = instance_eval(&page)
+
+        html = case result
+               when String
+                 result
+               else
+                 @agent.page.body
+               end
+      end
+
+      doc = Hpricot(html)
+
+      found_selectors = []
+
+      unused_selectors.each do |selector|
+        unless doc.search(selector).empty?
+          log.info("  #{selector}")
+          found_selectors << selector
+        end
+      end
+
+      unused_selectors -= found_selectors
+    end
+
+    log.info "found #{unused_selectors.size} unused selectors out of #{total_selectors} total"
+
+    unused_selectors
+  end
+
+  private
+
+  def agent
+    @agent ||= initialize_agent
+  end
+
+  def log
+    @log ||= Logger.new(@log_file)
+  end
+
+  def initialize_agent
+    begin
+      require 'mechanize'
+      return WWW::Mechanize.new
+    rescue LoadError
+      log.info %{
+        =================================================================
+        Couldn't load 'mechanize', which is required for remote scraping.
+        Install it like so: gem install mechanize
+        =================================================================
+      }
+
+      raise
+    end
+  end    
+
+  def fetch(path)
+    log.info(path)
+
+    loc = root + path
+
+    if @mechanize
+      loc = "file://#{File.expand_path(loc)}" unless loc =~ %r{^\w+://}
+      page = agent.get(loc)
+      log.warn("#{path} redirected to #{page.uri}") unless page.uri.to_s == loc
+      page.body
+    else
+      open(loc).read
+    end
+  end    
+end
+
--- test/deadweight_test.rb
+++ test/deadweight_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class DeadweightTest < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  def setup
+    @dw = Deadweight.new
+    @dw.log_file = 'test.log'
+    @dw.root = File.dirname(__FILE__) + '/fixtures'
+    @dw.stylesheets << '/style.css'
+    @dw.pages << '/index.html'
+
+    @result = @dw.run
+  end
+
+  should "report unused selectors" do
+    assert @result.include?('#foo .bar .baz')
+  end
+
+  should "not report used selectors" do
+    assert !@result.include?('#foo')
+    assert !@result.include?('#foo .bar')
+  end
+
+  should "accept Procs as targets" do
+    @dw.mechanize = true
+
+    @dw.pages << proc {
+      fetch('/index.html')
+      agent.page.links.first.click
+    }
+
+    assert @dw.run.empty?
   end
 end
