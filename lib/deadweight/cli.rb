96d415985a1af988dd125f440c09eca9b49906cd
Seth Fitzsimmons <seth@mojodna.net>
2009-08-30 22:21:51 -0700

    CLI and HTTP proxy
    Adds a command-line tool for scanning local files as well as an HTTP proxy.
    
    The HTTP proxy can be used by starting it up:
    
        $ deadweight -l dw.log -s styles.css -w http://mojodna.net/ -P
    
    And configuring Firefox (etc.) to use it ("Manual Proxy Configuration") before
    browsing through your site. It will helpfully display unmatched rules in your
    terminal window so you can see what to look for. `-w` configures a whitelist so
    you don't match your stylesheets against unexpected sites.
    
    Under OS X, you may be able to use [Lyndon](http://github.com/defunkt/lyndon)
    (with the `-L` option) to parse pages that contain HTML-modifying Javascript.

---
+++ lib/deadweight/cli.rb
@@ -0,0 +1,175 @@
+require 'optparse'
+require 'deadweight'
+
+class Deadweight
+  class CLI
+    attr_reader :stdout, :stdin, :stderr
+    attr_reader :arguments, :options
+    attr_reader :output
+
+    def self.execute(stdout, stdin, stderr, arguments = [])
+      @options = {
+        :log_file   => stderr,
+        :output     => stdout,
+        :proxy_port => 8002
+      }
+
+      self.parse_options(arguments)
+      self.new(stdout, stdin, stderr, arguments, @options).execute!
+    end
+
+    def self.option_parser
+      @option_parser ||= OptionParser.new do |opts|
+        opts.banner = "Usage: #{$0} [options] <url> [<url> ...]"
+
+        @options[:stylesheets] = []
+
+        opts.on("-L", "--lyndon", "Pre-process HTML with Lyndon") do
+          @options[:lyndon] = true
+        end
+
+        opts.on("-l", "--log FILE", "Where to write log messages") do |v|
+          @options[:log_file] = v
+        end
+
+        opts.on("-P", "--proxy", "Run in proxy mode") do
+          @options[:proxy] = true
+        end
+
+        opts.on("-p", "--proxy-port", "Port to run the proxy on") do |v|
+          @options[:proxy] = true
+          @options[:proxy_port] = v.to_i
+        end
+
+        opts.on("-O", "--output FILE",
+                "Where to output orphaned CSS rules") do |v|
+          @options[:output] = File.new(v, "w")
+        end
+
+        opts.on("-s", "--stylesheet FILE",
+                "Apply the specified stylesheet to the target") do |v|
+          @options[:stylesheets] << v
+        end
+
+        opts.on("-w", "--whitelist URL-PREFIX",
+                "Specifies a prefix for URLs to process") do |v|
+          @options[:whitelist] ||= []
+          @options[:whitelist] << v
+        end
+      end
+    end
+
+    def self.parse_options(arguments = [])
+      self.option_parser.parse!(arguments)
+      @options
+    end
+
+    def initialize(stdout, stdin, stderr, arguments = [], options = {})
+      @stdout = stdout
+      @stdin  = stdin
+      @stderr = stderr
+      @arguments = arguments
+      @options = options
+      @output = options[:output]
+    end
+
+    def execute!
+      if options[:proxy]
+        proxy
+      elsif arguments.empty?
+        stdout.puts self.class.option_parser.help
+      else
+        process
+      end
+    end
+
+    def process
+      # TODO pass stylesheets + pages as args
+      dw = Deadweight.new
+
+      # TODO this should be the default
+      dw.root = ""
+
+      dw.log_file = options[:log_file]
+
+      dw.stylesheets = options[:stylesheets]
+
+      dw.rules = stdin.read if stdin.stat.size > 0
+
+      if options[:lyndon]
+        arguments.each do |file|
+          dw.pages << IO.popen("cat #{file} | lyndon 2> /dev/null")
+        end
+      else
+        dw.pages = arguments
+      end
+
+      unused_rules = dw.run
+      unused_rules.each do |k,v|
+        output.puts "#{k} { #{v} }"
+      end
+    end
+
+    def proxy
+      dw = Deadweight.new
+
+      # TODO note the boilerplate shared with #process
+      dw.root = ""
+      dw.log_file = options[:log_file]
+      dw.stylesheets = options[:stylesheets]
+      dw.rules = stdin.read if stdin.stat.size > 0
+
+      # initialize selectors
+      dw.run
+
+      require 'webrick/httpproxy'
+
+      @proxy = WEBrick::HTTPProxyServer.new \
+        :AccessLog           => [
+          [options[:log_file], WEBrick::AccessLog::COMMON_LOG_FORMAT],
+          [options[:log_file], WEBrick::AccessLog::REFERER_LOG_FORMAT]
+        ],
+        :Logger              => WEBrick::Log.new(options[:log_file]),
+        :Port                => options[:proxy_port],
+        :ProxyContentHandler => lambda { |request, response|
+
+          parse_this = false
+
+          if options[:whitelist]
+            p options[:whitelist]
+            options[:whitelist].each do |x|
+              if response.request_uri.to_s[0..x.length - 1].downcase == x.downcase
+                parse_this = true
+                break 
+              end
+            end
+          else
+            parse_this = true
+          end
+
+          if parse_this
+            dw.process!(response.body)
+
+            stdout.puts "After reviewing <#{response.request_uri}>, these were left:"
+            dw.unused_selectors.each do |k,v|
+              stdout.puts "#{k} { #{v} }"
+            end
+          end
+        }
+
+      trap('INT') do
+        @proxy.shutdown 
+
+        # dump the remaining CSS rules if output is set
+        unless options[:output] == STDOUT
+          dw.unused_selectors.each do |k,v|
+            output.puts "#{k} { #{v} }"
+          end
+        end
+      end
+
+      @proxy.start
+    end
+  end
+end
+
